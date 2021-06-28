f794c85e23b9eb7991ee92d42952ebc09442f73c
Martin <martinplr@gmail.com>
2021-06-28 19:48:24 +0200

    style(TrayIndicator) format code

--- TrayIndicator.js
+++ TrayIndicator.js
@@ -1,39 +1,42 @@
-const Clutter		= imports.gi.Clutter;
-const GObject		= imports.gi.GObject;
-const St			= imports.gi.St;
-const PanelMenu		= imports.ui.panelMenu;
-const PopupMenu		= imports.ui.popupMenu
-const Me			= imports.misc.extensionUtils.getCurrentExtension();
-const AppManager	= Me.imports.AppManager;
-const GLib			= imports.gi.GLib;
-const getSettings	= imports.misc.extensionUtils.getSettings;
+const Clutter = imports.gi.Clutter;
+const GObject = imports.gi.GObject;
+const St = imports.gi.St;
+const PanelMenu = imports.ui.panelMenu;
+const PopupMenu = imports.ui.popupMenu;
+const Me = imports.misc.extensionUtils.getCurrentExtension();
+const AppManager = Me.imports.AppManager;
+const GLib = imports.gi.GLib;
+const getSettings = imports.misc.extensionUtils.getSettings;
 
 var TrayIndicator = new imports.lang.Class({
-	Name: 'TrayIndicator',
+	Name: "TrayIndicator",
 	Extends: PanelMenu.Button,
 
 	_init() {
 		this._icons = [];
 
 		this.parent(0.0, null, false);
-		this.style_class = 'panel-button TrayIndicator TrayIndicator--on-view';
+		this.style_class = "panel-button TrayIndicator TrayIndicator--on-view";
 		this._overflow = false;
 
 		this._indicators = new St.BoxLayout();
 		this.add_child(this._indicators);
 
 		this._icon = new St.Icon({
-			icon_name: 'view-more-horizontal',
-			style_class: 'system-status-icon',
+			icon_name: "view-more-horizontal",
+			style_class: "system-status-icon",
 			reactive: true,
 			track_hover: true,
-			visible: false
+			visible: false,
 		});
 		this._indicators.add_child(this._icon);
 
-		this._menuItem = new PopupMenu.PopupBaseMenuItem({ reactive: false, can_focus: true });
+		this._menuItem = new PopupMenu.PopupBaseMenuItem({
+			reactive: false,
+			can_focus: true,
+		});
 		this.menu.addMenuItem(this._menuItem);
-		this.menu.actor.add_style_class_name('TrayIndicatorPopup');
+		this.menu.actor.add_style_class_name("TrayIndicatorPopup");
 	},
 
 	get size() {
@@ -46,14 +49,19 @@ var TrayIndicator = new imports.lang.Class({
 		this._margin = margin;
 		this._padding = padding;
 
-		this._icons.forEach(icon => {
+		this._icons.forEach((icon) => {
 			icon.get_parent().style = this._getButtonStyle();
 			icon.set_size(this._size, this._size);
 		});
 	},
 
 	addIcon(icon) {
-		const button = new St.Button({ child: icon, button_mask: St.ButtonMask.ONE | St.ButtonMask.TWO | St.ButtonMask.THREE, style: this._getButtonStyle(), style_class: 'panel-button' });
+		const button = new St.Button({
+			child: icon,
+			button_mask: St.ButtonMask.ONE | St.ButtonMask.TWO | St.ButtonMask.THREE,
+			style: this._getButtonStyle(),
+			style_class: "panel-button",
+		});
 		icon.opacity = 0;
 		icon.set_x_align(Clutter.ActorAlign.CENTER);
 		icon.set_y_align(Clutter.ActorAlign.CENTER);
@@ -63,20 +71,28 @@ var TrayIndicator = new imports.lang.Class({
 			icon.ease({
 				opacity: 255,
 				duration: 400,
-				mode: Clutter.AnimationMode.EASE_OUT_QUAD
-			})
+				mode: Clutter.AnimationMode.EASE_OUT_QUAD,
+			});
 			this._addEffectIcon(icon);
 			return GLib.SOURCE_REMOVE;
 		});
-		icon.connect('destroy', () => { button.destroy(); });
+		icon.connect("destroy", () => {
+			button.destroy();
+		});
 
-		button.connect('button-release-event', (actor, event) => {
-			switch(event.get_button()) {
-				case 1: AppManager.toggleWindows(icon, event);		break;
-				case 2: AppManager.killWindows(icon, event);		break;
-				case 3: icon.click(event);							break;
+		button.connect("button-release-event", (actor, event) => {
+			switch (event.get_button()) {
+				case 1:
+					AppManager.toggleWindows(icon, event);
+					break;
+				case 2:
+					AppManager.killWindows(icon, event);
+					break;
+				case 3:
+					icon.click(event);
+					break;
 			}
-			if(AppManager.isWine(icon)) {
+			if (AppManager.isWine(icon)) {
 				GLib.timeout_add(GLib.PRIORITY_DEFAULT, 1, () => {
 					this.menu.close();
 					return GLib.SOURCE_REMOVE;
@@ -88,7 +104,7 @@ var TrayIndicator = new imports.lang.Class({
 
 		this._icons.push(icon);
 
-		if(this._overflow) {
+		if (this._overflow) {
 			this._menuItem.actor.add(button);
 		} else {
 			this._indicators.insert_child_at_index(button, 0);
@@ -97,45 +113,44 @@ var TrayIndicator = new imports.lang.Class({
 		this.checkOverflow();
 	},
 
-
 	removeIcon(icon, ignoreCheckOverflow) {
 		const index = this._icons.indexOf(icon);
-		this._icons.splice(index, 1);		
+		this._icons.splice(index, 1);
 
 		const actor = icon.get_parent();
 		actor.remove_actor(icon);
 		actor.destroy();
 
-		if(!ignoreCheckOverflow) {
+		if (!ignoreCheckOverflow) {
 			this.checkOverflow();
 		}
 	},
 
 	checkOverflow() {
-		if(this._icons.length >= getSettings().get_int('icons-limit')) {
+		if (this._icons.length >= getSettings().get_int("icons-limit")) {
 			this._overflow = true;
 			this._icon.visible = true;
 			this.reactive = true;
-			this.style_class = 'panel-button TrayIndicator TrayIndicator--overflow'; 
+			this.style_class = "panel-button TrayIndicator TrayIndicator--overflow";
 		} else {
 			this._overflow = false;
 			this._icon.visible = false;
 			this.reactive = false;
-			this.style_class = 'panel-button TrayIndicator TrayIndicator--on-view'; 
+			this.style_class = "panel-button TrayIndicator TrayIndicator--on-view";
 		}
 
-		if(this._icons.length) {
+		if (this._icons.length) {
 			this.visible = true;
 		} else {
 			this.visible = false;
 		}
-		
+
 		this._refreshIcons(this._overflow);
 	},
 
 	_refreshIcons(overflow) {
-		this._icons.forEach(icon => {
-			if(icon.inOverflow != overflow) {
+		this._icons.forEach((icon) => {
+			if (icon.inOverflow != overflow) {
 				this.removeIcon(icon, true);
 				this.addIcon(icon);
 			}
@@ -144,7 +159,7 @@ var TrayIndicator = new imports.lang.Class({
 
 	_getButtonStyle() {
 		let style;
-		if(!this._overflow) {
+		if (!this._overflow) {
 			style = `margin: ${this._margin.vertical}px ${this._margin.horizontal}px; padding: ${this._padding.vertical}px ${this._padding.horizontal}px`;
 		}
 		return `width: ${this.size}px; height: ${this.size}px;${style}`;
@@ -152,21 +167,29 @@ var TrayIndicator = new imports.lang.Class({
 
 	_addEffectIcon(icon) {
 		let brightnessContrast = new Clutter.BrightnessContrastEffect({});
-		brightnessContrast.set_contrast(getSettings().get_int('icon-contrast') / 100);
-		brightnessContrast.set_brightness(getSettings().get_int('icon-brightness') / 100);
-		icon.add_effect_with_name('brightnessContrast', brightnessContrast);
-		icon.add_effect_with_name('desaturate', new Clutter.DesaturateEffect({factor: getSettings().get_int('icon-saturation') / 100}));
+		brightnessContrast.set_contrast(
+			getSettings().get_int("icon-contrast") / 100
+		);
+		brightnessContrast.set_brightness(
+			getSettings().get_int("icon-brightness") / 100
+		);
+		icon.add_effect_with_name("brightnessContrast", brightnessContrast);
+		icon.add_effect_with_name(
+			"desaturate",
+			new Clutter.DesaturateEffect({
+				factor: getSettings().get_int("icon-saturation") / 100,
+			})
+		);
 	},
 
 	setEffect(contrast, saturation, brightness) {
-		this._icons.forEach(icon => {
-			let brightnessContrast = icon.get_effect('brightnessContrast');
+		this._icons.forEach((icon) => {
+			let brightnessContrast = icon.get_effect("brightnessContrast");
 			brightnessContrast.set_contrast(contrast / 100);
 			brightnessContrast.set_brightness(brightness / 100);
 
-			let desaturate = icon.get_effect('desaturate');
+			let desaturate = icon.get_effect("desaturate");
 			desaturate.set_factor(saturation / 100);
-
 		});
-	}
+	},
 });
